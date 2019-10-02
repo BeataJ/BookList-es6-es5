c41316b597b0d992b923eea34454f4089d6bafcf
beata <beata@emc2wave.com>
2019-10-02 15:25:06 -0700

    add value of title, author, isbn

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ function UI() {}
 
 // Event Listener
 document.getElementById('book-form').addEventListener('submit', function(e) {
-  console.log('test');
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;    
+        
+  console.log(title, author, isbn);      
 
   e.preventDefault();
 });

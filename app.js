bc2cf529093da64830a875436fe7addacf15e438
beata <beata@emc2wave.com>
2019-10-02 15:54:38 -0700

    add innerHtml to row appenedChild to list

--- app.js
+++ app.js
@@ -13,7 +13,15 @@ UI.prototype.addBookToList = function(book) {
   const list = document.getElementById('book-list');
   // Create Element
   const row = document.createElement('tr');
-  console.log(row);
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+
+  list.appendChild(row);
 }
 
 // Event Listener

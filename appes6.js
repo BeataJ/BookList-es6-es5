a68f38425baaa9cc3c4f41293d54fe616798a2cf
beata <beata@emc2wave.com>
2019-10-03 08:13:24 -0700

    add methods to UI

--- appes6.js
+++ appes6.js
@@ -7,5 +7,50 @@ class Book {
 }
 
 class UI {
-  
+  addBookToList(book) {
+    const list = document.getElementById("book-list");
+    // Create Element
+    const row = document.createElement("tr");
+    // Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+
+    list.appendChild(row);
+  }
+
+  showAlert(message, className) {
+    // Create div
+    const div = document.createElement("div");
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector(".container");
+    // Get form
+    const form = document.querySelector("#book-form");
+    // Insert alert
+    container.insertBefore(div, form);
+
+    // Timeout after 3s
+    setTimeout(function() {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
+
+  deleteBook(target) {
+    if (target.className === "delete") {
+      target.parentElement.parentElement.remove();
+    }
+  }
+
+  clearFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+  }
 }

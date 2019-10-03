85224aa5b0e15a62e583103c0021cd6de550d54b
beata <beata@emc2wave.com>
2019-10-02 17:22:49 -0700

    work on prototype deleteBook

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+// Delete Book
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById("title").value = "";
@@ -86,7 +93,15 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
 
 // Event Listener for delete
 document.getElementById('book-list').addEventListener('click', function(e) {
-  console.log(123);
+
+  // Instantiante UI
+  const ui = new UI();
+
+  // delete book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book Remove', 'success');
 
   e.preventDefault()
 });
